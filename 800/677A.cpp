b074e139d93b943b7bafe57f17128b2144ed0c1c
Abhijeet Biswas <abhijeet.24mca10070@vitbhopal.ac.in>
2024-10-05 21:16:26 +0530

    main

---
+++ 800/677A.cpp
@@ -0,0 +1,25 @@
+// Anaya and Fence  *800 
+
+#include <iostream>
+using namespace std;
+int main() {
+    int n , h ;
+    cin >> n >> h;
+    int arr[n];
+    int i;
+    for (i = 0; i < n ; i++) {
+        cin >> arr[i];
+    }
+    int width = 0;
+    for (i =0 ; i< n; i++) {
+        if (arr[i] <= h){
+            width +=1;
+        }
+        else{
+            width +=2;
+        }
+        cout << width;
+    }
+    return 0;
+
+}
