7c8ffb8f31137022fa5fcf7aa09e35597608fb4e
Abhijeet Biswas <abhijeet.24mca10070@vitbhopal.ac.in>
2024-10-05 21:17:48 +0530

    main

--- 800/677A.cpp
+++ 800/677A.cpp
@@ -22,3 +22,31 @@ int main() {
     cout << width;
 
 }
+=======
+// Anaya and Fence  800* 677A
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n , h;
+    cin >> n >> h;
+    int arr[n];
+    int i;
+    for (i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    int width = 0;
+    for (i= 0; i < n ; i++) {
+        if (arr[i] <= h){
+            width += 1;
+        }
+        else {
+            width += 2;
+        }
+        cout << width;
+        
+    }
+}
+    // Time - O(n)
+>>>>>>> Stashed changes
